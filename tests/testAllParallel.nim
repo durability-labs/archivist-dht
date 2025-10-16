bfadff1b116f9891562361ead27424c2431f71bb
Ben <thatbenbierens@gmail.com>
2025-10-16 10:08:12 +0200

    Adds system testing option to enable send failure probability

---
+++ tests/testAllParallel.nim
@@ -0,0 +1,23 @@
+# import
+#   ./dht/[test_providers, test_providermngr],
+#   ./discv5/[test_discoveryv5, test_discoveryv5_encoding]
+
+import osproc
+
+var cmds: seq[string]
+
+when defined(testsPart1) or defined(testsAll):
+  cmds.add [
+    "nim c -r --hints:off --verbosity:0 dht/test_providers.nim",
+    "nim c -r --hints:off --verbosity:0 dht/test_providermngr.nim",
+  ]
+when defined(testsPart2) or defined(testsAll):
+  cmds.add [
+    "nim c -r --hints:off --verbosity:0 discv5/test_discoveryv5.nim",
+    "nim c -r --hints:off --verbosity:0 discv5/test_discoveryv5_encoding.nim",
+    "nim c -r --hints:off --verbosity:0 -d:archivist_system_testing_options discv5/test_system_testing_option.nim"
+  ]
+
+echo "Running Test Commands: ", cmds
+
+quit execProcesses(cmds)
